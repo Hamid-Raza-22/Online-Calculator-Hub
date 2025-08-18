e9d8fccba5a03f1e9b7be871aeeae3c091d03878
Hamid Raza <hamidraza.engr@gmail.com>
2025-08-18 12:47:44 +0500

    Project Structure

---
+++ lib/screens/splash/splash_controller.dart
@@ -0,0 +1,36 @@
+// import 'dart:async';
+//
+// import 'package:assaan_rishta/app/core/routes/app_routes.dart';
+// import 'package:get/get.dart';
+//
+// import '../../core/base/export.dart';
+// import '../../domain/export.dart';
+//
+//
+//
+//
+//
+// class SplashController extends BaseController {
+//   UserManagementUseCase useCase;
+//
+//   SplashController(this.useCase);
+//
+//   @override
+//   void onInit() async {
+//     super.onInit();
+//     bool status = useCase.userManagementRepo.getUserLoggedInStatus();
+//     if (status) {
+//       Timer(
+//         const Duration(seconds: 2),
+//         () {
+//            Get.offNamed(AppRoutes.BOTTOM_NAV);
+//           //   () => const BottomNavView(),
+//           //   binding: AppBindings(),
+//           //   transition: Transition.circularReveal,
+//           //   duration: const Duration(milliseconds: 500),
+//           // );
+//         },
+//       );
+//     }
+//   }
+// }
